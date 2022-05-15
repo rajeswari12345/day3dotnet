a641aa499709d6b83d61e5a44c07ee1b2cd54fec
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-15 18:41:42 +0530

    day learning uc1

---
+++ day3 uc1 learning.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        int ITS_FULL_TIME = 1;
+
+        Random random = new Random();
+        //Computation;
+        int empCheck = random.Next(0, 2);
+        if (empCheck == ITS_FULL_TIME)
+        {
+            Console.WriteLine("Employee is Present");
+        }
+        else
+        {
+            Console . WriteLine("Employee is present");
+        }
+    }
+
+}
